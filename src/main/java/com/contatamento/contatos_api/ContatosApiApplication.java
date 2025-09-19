db66add8350bacb9dabc9f615492725248d03747
skbruno <bruno.rodriguesc9@gmail.com>
2025-09-18 21:47:34 -0300

    3

---
+++ src/main/java/com/contatamento/contatos_api/ContatosApiApplication.java
@@ -0,0 +1,13 @@
+package com.contatamento.contatos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contatamento/contatos_api/ContatosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contatamento.contatos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContatosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
